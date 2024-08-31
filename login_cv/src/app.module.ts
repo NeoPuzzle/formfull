0d4f95c997c683ecb7a47f854f6cbaeef351e638
rbenites <alrrebe@gmail.com>
2024-08-30 22:03:45 -0500

    typeorm config, database config

--- login_cv/src/app.module.ts
+++ login_cv/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      load: [typeOrmConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: (configService: ConfigService) => configService.get('typeorm')
+    }),
+    AuthModule,
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '1h'},
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
