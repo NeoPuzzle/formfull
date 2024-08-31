68f4c670de2646c0b92e8f6199571c944e1e1eab
rbenites <alrrebe@gmail.com>
2024-08-30 22:12:29 -0500

    logger middleware

--- login_cv/src/main.ts
+++ login_cv/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { loggerGlobal } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(loggerGlobal)
   await app.listen(3000);
+  console.log(`Server listening on port 3000`);
 }
 bootstrap();
